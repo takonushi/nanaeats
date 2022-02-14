43af0cc749ae43dea4da38f2c6b3733f6a2a84a0
takonushi <hewitt0120@gmail.com>
2022-02-14 17:32:14 +0900

    注文機能及びDBへの更新処理まで実装
    orders/index.htmlビューの作成
    index,createアクションの実装・DB更新出来るところまで確認済み。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   def index
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+end
+
+private
+def set_item
+  @item = Item.find(params[:item_id])
+end
+
+def order_params
+  params.require(:order).permit(:order_comment).merge(user_id: current_user.id, item_id: @item.id, order_date: Date.today, order_status: 1)
 end
