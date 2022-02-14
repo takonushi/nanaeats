e77bd95f2ca87e9c21cae7a4cfd8e44b5332c18f
takonushi <hewitt0120@gmail.com>
2022-02-14 16:34:49 +0900

    order関連の設定追加
    ・ordersコントローラー作成
    ・ルーティング(ネスト)記述
    ・orderモデル作成
    ・orderテーブル作成

--- README.md
+++ README.md
@@ -30,7 +30,7 @@
 | -------------------- | ---------- | ------------------------------ |
 | order_date           | date       | null: false                    |
 | order_comment        | text       |                                |
-| used_item            | integer    | null: false
+| order_status         | integer    | null: false
 | user                 | references | null: false, foreign_key: true |
 | item                 | references | null: false, foreign_key: true |
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_132224) do
+ActiveRecord::Schema.define(version: 2022_02_13_095400) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(version: 2022_01_31_132224) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "order_date", null: false
+    t.text "order_comment"
+    t.integer "order_status", null: false
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -60,4 +72,6 @@ ActiveRecord::Schema.define(version: 2022_01_31_132224) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
