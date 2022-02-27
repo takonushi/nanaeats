93dbe53c5d024218885957a55b3afc0459a74b61
takonushi <hewitt0120@gmail.com>
2022-02-27 12:24:56 +0900

    Formオブジェクト(item_order)の作成・実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create, :edit]
+  before_action :set_order, only: [:edit, :update]
   def index
     @order = Order.new
   end
@@ -15,7 +16,18 @@ class OrdersController < ApplicationController
   end
 
   def edit
-    @order = Order.find(params[:id])
+    order_attributes = @order.attributes
+    @item_order = ItemOrder.new(order_attributes)
+  end
+
+  def update
+    @item_order = ItemOrder.new(item_order_params)
+    if @item_order.valid?
+      @item_order.update(item_order_params, @order)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
 end
@@ -25,6 +37,14 @@ def set_item
   @item = Item.find(params[:item_id])
 end
 
+def set_order
+  @order = Order.find(params[:id])
+end
 def order_params
   params.require(:order).permit(:order_comment).merge(user_id: current_user.id, item_id: @item.id, order_date: Date.today, order_status_id: 1)
 end
+
+def item_order_params
+  params.require(:item_order).permit(:quantity, :order_date, :order_comment, :order_status_id)
+end
+
--- app/models/order_status.rb
+++ app/models/order_status.rb
@@ -3,6 +3,7 @@ class OrderStatus < ActiveHash::Base
     {id: 1, name: '注文依頼中' },
     {id: 2, name: '注文承諾済み' },
     {id: 3, name: '調理完了済み' },
+    {id: 4, name: 'キャンセル処理' },
   ]
   include ActiveHash::Associations
     has_many :orders
