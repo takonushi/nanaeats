834d33545844412230159c006a9d65f97f947905
takonushi <hewitt0120@gmail.com>
2022-03-03 16:23:11 +0900

    ログイン、itemsテーブルに対してのエラーメッセージの日本語化、単体テストコード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,9 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :quantity,
-              numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100, message: 'is invalid' }
-    validates :image
+              numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100, message: 'は0~100の値を半角数値で入力してください' }
+    validates :image, presence: { message: 'を選択してください'}
   end
   validates :price,
-            numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10_000, message: 'is invalid' }, allow_nil: true
+            numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10_000, message: 'は1~10000の値を半角数値で入力してください' }, allow_nil: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,37 +21,54 @@ RSpec.describe Item, type: :model do
         @item.explanation = ''
         expect(@item).to be_valid
       end
+
+      it '購入先は空でも登録ができる' do
+        @item.retailer = ''
+        expect(@item).to be_valid
+      end
     end
 
     context '内容に問題がある場合' do
       it '商品名が空では登録ができない' do
         @item.name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
 
       it '商品画像が未登録では登録ができない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
+        expect(@item.errors.full_messages).to include("商品画像を選択してください")
       end
 
       it '金額に半角数値以外の値が入っている場合登録ができない' do
         @item.price = '10,000'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is invalid')
+        expect(@item.errors.full_messages).to include("金額は1~10000の値を半角数値で入力してください")
       end
 
       it '金額に10000を超える値が入っている場合登録ができない' do
         @item.price = '30000'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is invalid')
+        expect(@item.errors.full_messages).to include("金額は1~10000の値を半角数値で入力してください")
+      end
+
+      it '在庫に半角数値以外の値が入っている場合は登録ができない' do
+        @item.quantity = '十六'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("在庫は0~100の値を半角数値で入力してください")
+      end
+
+      it '在庫に100を超える値が入っている場合登録ができない' do
+        @item.quantity = '200'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("在庫は0~100の値を半角数値で入力してください")
       end
 
       it 'ユーザーが紐づいていない場合登録ができない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
