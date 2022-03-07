dfcf7fccd3ad417639179761de5d4bcb881f024f
takonushi <hewitt0120@gmail.com>
2022-03-07 22:32:57 +0900

    商品分類の修正

--- app/models/item_class.rb
+++ app/models/item_class.rb
@@ -1,12 +1,11 @@
 class ItemClass < ActiveHash::Base
   self.data = [
     { id: 0, name: 'その他' },
-    { id: 1, name: 'ご飯' },
-    { id: 2, name: '汁物' },
-    { id: 3, name: '麺類' },
-    { id: 4, name: '肉' },
-    { id: 5, name: '魚' },
-    { id: 6, name: 'サブ料理' },
+    { id: 1, name: 'メイン料理' },
+    { id: 2, name: 'サブ料理' },
+    { id: 3, name: '汁物' },
+    { id: 4, name: '飲み物' },
+    { id: 5, name: '雑用' },
   ]
   include ActiveHash::Associations
   has_many :items
