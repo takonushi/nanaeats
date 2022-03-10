6b7428200315f87c95ad8a0d159fc37961a8fd97
takonushi <hewitt0120@gmail.com>
2022-03-10 15:55:21 +0900

    orderテーブルに対しての単体テストコード実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,11 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
 
+  with_options presence: true do
+    validates :order_date
+    validates :order_status_id
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :order_status
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :order do
+    order_date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
+    order_comment { Faker::Lorem.sentence }
+    order_status_id { Faker::Number.between(from: 1, to: 4) }
+    association :item
+    association :user
   end
 end
